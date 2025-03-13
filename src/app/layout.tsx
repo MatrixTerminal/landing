36b84d2ad06eaf5161f63cdbdd98ba1418a06138
vidvidvid <weetopol@gmail.com>
2025-03-13 07:32:37 +0100

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,18 @@
-"use client";
+import './globals.css';
 
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+export const metadata = {
+  title: 'Matrix - The Matrix',
+  description: 'Enter The Matrix',
+};
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="dark">
-      <head>
-        <title>Matrix - The Matrix</title>
-        <meta name="description" content="Enter The Matrix" />
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} dark:bg-black dark:text-white`}
-      >
+      <body className="dark">
         {children}
       </body>
     </html>
